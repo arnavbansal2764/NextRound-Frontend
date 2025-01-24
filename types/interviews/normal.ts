ca78b5354e087d583d78f6ba62a3ee20b8b2995d
Dushyant Bhardwaj <dushyantbhardwaj@AirStalin.local>
2025-01-24 11:26:55 +0530

    addd scores

--- types/interviews/normal.ts
+++ types/interviews/normal.ts
@@ -46,13 +46,20 @@ export interface InitialSetupMessage {
     action: "analyze";
   }
   
+  export interface Score {
+    question: string;
+    answer: string;
+    referenceAnswer: string;
+    score: number;
+}
+
   // Analyze responses response
   export interface AnalyzeResponse {
     status: "ok" | "error";
     code: number;
     action: "analyze";
     analysis: string;
-    scores: any;
+    scores: Score[];
     averageScore: number;
     totalScore: number;
   }
